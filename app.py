f7692751fe1f729b412c4f86c236e97dc71cdbf2
subh2981 <subhamdas2981@gmail.com>
2026-01-10 13:27:42 +0530

    DataIngestion

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 import sys
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+
 if __name__=="__main__":
     logging.info("The Execution has started")
     
     try:
-        a=1/0
         
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initialte_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
