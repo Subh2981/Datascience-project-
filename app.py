29abd66cc422f736a26d510244863f94b7b900e0
subh2981 <subhamdas2981@gmail.com>
2026-01-10 20:00:09 +0530

    untrack

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 import sys
 from src.mlproject.components.data_ingestion import DataIngestion
-from src.mlproject.components.data_ingestion import DataIngestionConfig
+#from src.mlproject.components.data_ingestion import DataIngestionConfig
 
 if __name__=="__main__":
     logging.info("The Execution has started")
